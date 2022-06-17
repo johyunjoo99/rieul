0e1bf1c321efad95a0bca06d3f165a71adbf459b
johyunjoo99 <chj17200@naver.com>
2022-06-17 19:00:12 +0900

    RIEUL_2022.06.17
    메인 페이지 갤러리 hover effect & 팝업구현

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,27 @@
 $(function(){
 
+
+    // 팝업 block
+    $(".zoom").click(function(){
+        $("#popup").fadeIn();
+    });
+
+    $(".close").click(function(){
+        $("#popup").fadeOut();
+    });
+
+    
+
+
+    // 팝업 슬라이드 hover effect
+    $(".noClick").mouseenter(function(){
+        $(this).hide();
+    });
+
+    $(".rows > img").mouseleave(function(){
+        $(this).prev(".noClick").show();
+    });
+
+
+    
 });
