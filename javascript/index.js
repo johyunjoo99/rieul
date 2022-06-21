8881e6d18f3c8ccf1817f4424406b37bbc26e098
johyunjoo99 <chj17200@naver.com>
2022-06-21 19:48:34 +0900

    RIEUL_2022.06.21
    메인 페이지 팝업창 구현 & 슬라이드 구현

--- javascript/index.js
+++ javascript/index.js
@@ -2,8 +2,12 @@ $(function(){
 
 
     // 팝업 block
-    $(".zoom").click(function(){
+    $(".container").click(function(){
         $("#popup").fadeIn();
+
+        $("#slide_img img").attr("src","img/index_img/popup" + ($(this).index()+1) + ".jpg");
+        $(".rows").children(".noClick").removeClass("active");
+        $(".rows").eq($(this).index()).children(".noClick").addClass("active");
     });
 
     $(".close").click(function(){
@@ -14,14 +18,27 @@ $(function(){
 
 
     // 팝업 슬라이드 hover effect
-    $(".noClick").mouseenter(function(){
-        $(this).hide();
+    $(".rows").click(function(){
+
+    }); 
+
+    $(".rows").click(function(){
+        $("#slide_img img").attr("src", "img/index_img/popup" + ($(this).index()+1) + ".jpg")
+        $(".noClick").removeClass("active");
+        $(this).children(".noClick").addClass("active");
     });
 
-    $(".rows > img").mouseleave(function(){
-        $(this).prev(".noClick").show();
-    });
 
+    // 스폰서 슬라이드
+    var fullWidth = $("#slide").width();
+    var slideWidth = fullWidth / 6
 
-    
+    function sponsor(){
+        $("#slide").stop().animate({marginLeft: -slideWidth}, function(){
+            $(".slideImg:first").appendTo("#slide");
+            $("#slide").css({marginLeft: 0});
+        });
+    }
+
+    setInterval(sponsor, 5000);
 });
